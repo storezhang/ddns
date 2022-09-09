c77f8f4ef87a6754504bc022b16a1a37346a5a5e
storezhang <storezhang@gmail.com>
2022-09-09 20:05:25 +0800

    feat(命令行): 增加名称、作者以及使用说明

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 
 func main() {
 	panic(pangu.New(
+		pangu.Named(`ddns`),
 		pangu.Banner(`DDNS`, pangu.BannerTypeAscii),
+		pangu.Author(`storezhang`, `storezhang@gmail.com`),
+		pangu.Usage(`动态载解析，使用请参数https://github.com/storezhang/ddns的相关说明`),
 	).Run(newBootstrap))
 }
