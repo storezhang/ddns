16de65c92c364f49bcf469455c44159b1e3c59be
storezhang <storezhang@gmail.com>
2022-09-12 10:03:20 +0800

    refactor(配置): 操作间隔改为15s

--- conf/ddns.go
+++ conf/ddns.go
@@ -5,5 +5,5 @@ import "time"
 type ddns struct {
 	// 间隔
 	// nolint:lll
-	Interval time.Duration `default:"5s" json:"interval" yaml:"interval" xml:"interval" toml:"interval" validate:"required"`
+	Interval time.Duration `default:"15s" json:"interval" yaml:"interval" xml:"interval" toml:"interval" validate:"required"`
 }
